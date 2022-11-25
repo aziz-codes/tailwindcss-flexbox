0b5def17c99de041ac81c9645c34b9a40f23257e
aziz-codes <azizisoffline@gmail.com>
2022-11-25 14:52:21 +0500

    Responsive Bottom Navigation Menu

--- src/components/BottomNavigation.jsx
+++ src/components/BottomNavigation.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-
+import {
+  HomeIcon,
+  HeartIcon,
+  UserIcon,
+  MagnifyingGlassIcon,
+  ClockIcon,
+} from "@heroicons/react/24/outline";
 const BottomNavigation = () => {
   return (
-    <div className="fixed md:hidden bottom-0 h-10 shadow-md border  w-full">
-      <div className="w-screen flex justify-between pl-2 pr-2">
-        <p>icon1 </p>
-        <p>icon2 </p>
-        <p>icon3 </p>
-        <p>icon4 </p>
-        <p>icon5 </p>
+    <div className="fixed md:hidden bottom-0 h-10 shadow-md border w-full">
+      <div className="w-screen flex flex-row h-full justify-between pl-2 pr-2 items-center">
+        <HomeIcon className="icons" />
+        <HeartIcon className="icons" />
+        <MagnifyingGlassIcon className="icons" />
+        <UserIcon className="icons" />
+        <ClockIcon className="icons" />
       </div>
     </div>
   );
